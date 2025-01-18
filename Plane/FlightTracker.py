b64fd9114631feabff1bd37ef0535f27854c92ae
IsaacVerbrugge25 <personontheinternet1234@gmail.com>
2025-01-17 23:40:35 -1000

    added flighttracker

--- Plane/FlightTracker.py
+++ Plane/FlightTracker.py
@@ -1,6 +1,27 @@
 import threading
+import datetime
 
 class FlightTracker:
 
-    def __init__(self):
-        ...
+    def __init__(self, imu, gps):
+        self.imu = imu
+        self.gps = gps
+
+        self.data_file = f"{str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}.csv"
+        with open(self.data_file, 'a') as file:
+            file.write(f"time,pitch,heading,roll,latitude,longitude")
+
+    def start_threads(self):
+        threading.Thread(target=self.log).start()
+
+    def log(self):
+        while True:
+            with open(self.data_file, 'a') as file:
+                file.write(f"{datetime.datetime.now().strftime()},{self.imu.pitch},{self.imu.tiltCompensatedHeading},{self.imu.roll},{self.gps.latitude},{self.gps.longitude}")
+                # file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')},pitch,heading,roll,latitude,longitude\n")
+
+if __name__ == "__main__":
+    f = FlightTracker("","")
+    f.start_threads()
+
+
--- Plane/MainPlane.py
+++ Plane/MainPlane.py
@@ -9,20 +9,32 @@ import threading
 from ServoController import ServoController
 from PlaneCommunicationHandler import CommunicationHandler
 from IMU.IMUProcessor import IMUProcessor
+from Navigation.I2CGPS import I2CGPS
+from Navigation.NavigationManager import NavigationManager
+from FlightTracker import FlightTracker
 from Camera import Camera
 from time import sleep
 
 if __name__ == "__main__":
     try:
-        IMU = IMUProcessor()
-        IMU.setup()
-        IMU.start_threads()
+        imu = IMUProcessor()
+        imu.setup()
+        imu.start_threads()
 
-        servoController = ServoController(IMU)
+        servoController = ServoController(imu)
         servoController.setup_servos()
         servoController.start_threads()
 
         communicationHandler = CommunicationHandler(servoController)
+
+        # gps = I2CGPS()
+        # gps.start_threads()
+
+        # navigationManager = NavigationManager(servoController, imu, gps)
+        # navigationManager.start_threads()
+
+        # flightTracker = FlightTracker(imu, gps)
+        # flightTracker.start_threads()
 
         # camera = Camera(communicationHandler)
         # camera.start_threads()
